c975b829c17ea1d63e4f6a72e608ab144de0b18f
Demonrux <demonrux201@gmail.com>
2026-04-11 02:51:18 +0300

    refactor: update IUserService to use ServiceResult

--- StudentCouncil.Logic/Interfaces/IUserService.cs
+++ StudentCouncil.Logic/Interfaces/IUserService.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
-using StudentCouncil.Logic.DTOs.UsersDTOs;
+using StudentCouncil.Logic.DTOs;
+using StudentCouncil.Logic.Services;
 using System.Security.Claims;
-using static StudentCouncil.Logic.Services.UserService;
 
 namespace StudentCouncil.Logic.Interfaces;
 
 public interface IUserService
 {
-    Task<List<UserDTO>>? GetAllUsersWithRolesAsync();
-    Task<UserDTO?> GetUserByIdAsync(int id);
-    Task<(UserDTO? User, ProfileAccessResult Result)> GetUserProfileAsync(int id, ClaimsPrincipal currentUser);
-    Task<bool> CreateUserAsync(CreateUserDTO dto, string password);
-    Task<bool> UpdateUserAsync(int id, UpdateUserDTO dto, ClaimsPrincipal currentUser);
-    Task<bool> DeleteUserAsync(int id, ClaimsPrincipal currentUser);
-    Task<bool> UpdateAvatarAsync(int userId, IFormFile avatar, ClaimsPrincipal currentUser);
-    Task<bool> DeleteAvatarAsync(int userId, ClaimsPrincipal currentUser);
+    Task<ServiceResult<List<UserDTO>>> GetAllUsersAsync();
+    Task<ServiceResult<UserDTO>> GetUserByIdAsync(int id);
+    Task<ServiceResult<UserDTO>> GetUserProfileAsync(int id, ClaimsPrincipal currentUser);
+    Task<ServiceResult> CreateUserAsync(CreateUserDTO dto, string password);
+    Task<ServiceResult> UpdateUserAsync(int id, UpdateUserDTO dto, ClaimsPrincipal currentUser);
+    Task<ServiceResult> DeleteUserAsync(int id, ClaimsPrincipal currentUser);
+    Task<ServiceResult> UpdateAvatarAsync(int userId, IFormFile avatar, ClaimsPrincipal currentUser);
+    Task<ServiceResult> DeleteAvatarAsync(int userId, ClaimsPrincipal currentUser);
 }
