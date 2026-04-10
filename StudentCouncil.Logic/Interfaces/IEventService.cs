f8254b42029f7545d45fb4345990daf1002c4277
Demonrux <demonrux201@gmail.com>
2026-04-11 02:51:06 +0300

    feat: add IEventService interface

---
+++ StudentCouncil.Logic/Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using StudentCouncil.Logic.DTOs;
+using StudentCouncil.Logic.Services;
+
+namespace StudentCouncil.Logic.Interfaces;
+
+public interface IEventService
+{
+    Task<ServiceResult<EventListResponseDTO>> GetAllEventsAsync();
+    Task<ServiceResult<EventResponseDTO>> GetEventByIdAsync(int id);
+    Task<ServiceResult<EventResponseDTO>> CreateEventAsync(CreateEventDTO dto, int currentUserId);
+    Task<ServiceResult<EventResponseDTO>> UpdateEventAsync(int id, UpdateEventDTO dto, int currentUserId);
+    Task<ServiceResult> DeleteEventAsync(int id, int currentUserId);
+}
