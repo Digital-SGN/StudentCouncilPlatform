9129d743d244892250f6a0bc7ab2d43a43a1f789
Demonrux <demonrux201@gmail.com>
2026-04-11 02:50:47 +0300

    feat: add Event DTOs (CreateEventDTO, UpdateEventDTO, EventResponseDTO, EventListResponseDTO)

---
+++ StudentCouncil.Logic/DTOs/EventDTOs.cs
@@ -0,0 +1,44 @@
+﻿using StudentCouncil.Data.Models;
+
+namespace StudentCouncil.Logic.DTOs
+{
+    public class CreateEventDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description {  get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string RegistrationLink {  get; set; } = string.Empty;
+        public int ResponsibleUserId { get; set; }
+    }
+
+    public class UpdateEventDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string RegistrationLink { get; set; } = string.Empty;
+        public int ResponsibleUserId { get; set; }
+        public EventStatus Status { get; set; }
+    }
+
+    public class EventResponseDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string RegistrationLink { get; set; } = string.Empty;
+        public EventStatus Status { get; set; }
+        public int ResponsibleUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class EventListResponseDTO
+    {
+        public int Count { get; set; }
+        public List<EventResponseDTO> Events { get; set; } = [];
+    }
+}
